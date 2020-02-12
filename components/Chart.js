350d4c97c6116c898b8e83cca5ad56e4bd03e186
Glitch (0sumrich-team-plan) <none>
2020-02-12 14:50:06 +0000

    🐓🖼 Checkpoint ./components/Chart.js:692249/2458

--- components/Chart.js
+++ components/Chart.js
@@ -3,7 +3,7 @@ import draw from "../helper/draw";
 import * as d3 from "d3";
 
 function darker(col) {
-  return d3.color(col).darker([2]) + "";
+  return d3.color(col).darker([0.5]);
 }
 
 function Chart({ data, edit }) {
@@ -13,9 +13,26 @@ function Chart({ data, edit }) {
       d3.selectAll('g[class^="arc"]')
         .style("cursor", "pointer")
         .on("mouseover", function() {
-          const e = d3.select(this.firstElementChild).style('fill');
-          debugger;
-          e.style("fill", "#ffffff");
+          const e = d3.select(this.firstElementChild);
+          const color = d3.rgb(e.style("fill"));
+          e.style(
+            "fill",
+            d3
+              .color(color)
+              .darker([1])
+              .rgb()
+          );
+        })
+        .on("mouseout", function() {
+          const e = d3.select(this.firstElementChild);
+          const color = d3.rgb(e.style("fill"));
+          e.style(
+            "fill",
+            d3
+              .color(color)
+              .brighter([1])
+              .rgb()
+          );
         })
         .on("click", e => {
           debugger;
