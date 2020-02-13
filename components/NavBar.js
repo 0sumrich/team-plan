013404b8d89cf5523ea4668d342c859a49ac263c
Richard Lynch <lynchardrich@gmail.com>
2020-02-13 20:11:12 +0000

    mui give in

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,26 +1,66 @@
 import React, { Fragment } from "react";
-import NavTab from "./NavTab";
+import { makeStyles } from "@material-ui/core/styles";
+// import NavTab from "./NavTab";
+import Menu from "@material-ui/core/Menu";
 import Button from "./Button";
+import AppBar from "@material-ui/core/AppBar";
+import Container from "@material-ui/core/Container";
 import { saveSvgAsPng } from "save-svg-as-png";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
-function exportPng(){
+const useStyles = makeStyles(theme => {
+  return {
+    root: {
+      flexGrow: 1
+    },
+    active: {
+      background: theme.palette.grey[200]
+    },
+    ul: {
+      listStyleType: "none"
+    },
+    li: {
+      display: "inline",
+      padding: theme.spacing(2),
+    }
+  };
+});
+
+function exportPng() {
   return saveSvgAsPng(document.getElementById("svg"), "diagram.png", {
-      scale: 8,
-      backgroundColor: "white"
-    });
+    scale: 8,
+    backgroundColor: "white"
+  });
 }
 
 const NavBar = () => {
+  const classes = useStyles();
+
+  const NavTab = ({ to, children }) => {
+    const router = useRouter();
+    const active = to == router.pathname ? "active" : "";
+    return (
+      <li className={`${classes.li} ${active}`}>
+        <Link href={to}>
+          <a>{children}</a>
+        </Link>
+      </li>
+    );
+  };
   return (
-    <nav>
-      <div className="nav-wrapper">
-        <ul id="nav-mobile">
-          <NavTab to="/">Home</NavTab>
-          <NavTab to="/edit">Edit</NavTab>
-          <li><a className="btn" onClick={() => exportPng()}>Export as PNG</a></li>
-        </ul>
-      </div>
-    </nav>
+    <div className={classes.root}>
+      <AppBar position="static" color="transparent">
+        <Container>
+          <ul className={classes.ul}>
+            <NavTab to="/">Home</NavTab>
+            <NavTab to="/edit">Edit</NavTab>
+          </ul>
+        </Container>
+      </AppBar>
+    </div>
   );
 };
 
---
+++ styles/theme.js
@@ -0,0 +1,21 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#3f51b5"
+    },
+    secondary: {
+      main: "#f50057"
+    },
+    error: {
+      main: "#f44336"
+    },
+    background: {
+      default: "#fafafa"
+    }
+  }
+});
+
+export default theme;
